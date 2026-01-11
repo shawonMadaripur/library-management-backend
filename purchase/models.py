1c92f8bff9b495ebc9ea96be2e1c5b4756fdf2c8
Mehedi Hasan Shuvo <102133774+shawonMadaripur@users.noreply.github.com>
2026-01-11 18:01:21 +0600

    change purchase info table

--- purchase/models.py
+++ purchase/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Purchase_info(models.Model):
-    names = models.CharField(max_length=300)
+    book_name = models.CharField(max_length=300)
     student_id = models.IntegerField()
-    when_purchased = models.DateTimeField(auto_now_add=True)
-    delivery_time = models.DateTimeField()
+    picup_time = models.DateField(blank=True, null=True)
+    delivery_time = models.DateField(blank=True, null=True)
